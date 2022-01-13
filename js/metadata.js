6e93252370906c0535dd5ac5681e1b68a6f71862
KenanTurner <coolspykee@satx.rr.com>
2022-01-12 23:41:39 -0600

    metadata now uses a cool proxy

--- js/metadata.js
+++ js/metadata.js
@@ -1,9 +1,9 @@
 import Album from './custom-album.js';
-let metadata = (await (await fetch("../php/metadata.php")).json()).reduce(function(arr,obj){
+let arr = (await (await fetch("../php/metadata.php")).json()).reduce(function(arr,obj){
 	try{
 		arr.push(new Album(obj));
 	}catch(e){
-		console.error("Failed to initialize album: ",obj);
+		console.error(e,obj);
 	}
 	return arr;
 },[]).filter(function(album){
@@ -13,5 +13,24 @@ let metadata = (await (await fetch("../php/metadata.php")).json()).reduce(functi
 	if(a.title > b.title) return 1;
 	if(a.title === b.title) return 0;
 });
+let metadata = new Proxy(arr,{
+	set: function(target, property, value, receiver){
+		let res = target[property] = value;
+		//localStorage.setItem(name,JSON.stringify(target));
+		target.sort(function(a,b){
+			if(a.title < b.title) return -1;
+			if(a.title > b.title) return 1;
+			if(a.title === b.title) return 0;
+		});
+		let album_container = document.getElementById('album-container');
+		while(album_container.firstChild) album_container.lastChild.remove();
+		target.forEach(function(album){
+			album_container.appendChild(album.toNode());
+		});
+		return res;
+	}
+});
 window.metadata = metadata;
+
+console.log("Metadata Loaded");
 export default metadata;
