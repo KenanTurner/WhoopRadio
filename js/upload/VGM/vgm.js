9b7745574fcb89d8af3e866e7a7cce048f574be6
KenanTurner <coolspykee@satx.rr.com>
2022-02-12 13:28:23 -0600

    VGM now uses AsyncQueue

--- js/upload/VGM/vgm.js
+++ js/upload/VGM/vgm.js
@@ -1,5 +1,7 @@
 import _HTML from '../HTML/html.js';
+import AsyncQueue from '../../async-queue.js';
 export default class VGM extends _HTML{
+	static MAX_CONCURRENT_REQUESTS = 6;
 	static async fetchTrack(track){
 		let response = await fetch('./js/upload/VGM/fetch-track.php',{
 			method: 'POST',
@@ -15,8 +17,9 @@ export default class VGM extends _HTML{
 		});
 		if(!response.ok) throw new Error("Fetch Failed!");
 		let obj = await response.json();
+		let queue = new AsyncQueue(VGM.MAX_CONCURRENT_REQUESTS);
 		let parr = obj.tracks.map(function(track){
-			return VGM.fetchTrack(track);
+			return queue.enqueue(VGM.fetchTrack,track);
 		});
 		obj.tracks = (await Promise.allSettled(parr)).reduce(function(arr,p,i){
 			if(p.status !== "rejected" && p.value) arr.push(p.value);
